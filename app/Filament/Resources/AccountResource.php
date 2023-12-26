229672c700099963e1815dd5f5dddb9d4c266d2d
relliv <epsilonelittr@gmail.com>
2023-12-26 04:19:27 +0300

    Add account resource

---
+++ app/Filament/Resources/AccountResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AccountResource\Pages;
+use App\Filament\Resources\AccountResource\RelationManagers;
+use App\Models\Account;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AccountResource extends Resource
+{
+    protected static ?string $model = Account::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('number')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('currency_code')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('opening_balance')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('bank_name')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('bank_phone')
+                    ->tel()
+                    ->maxLength(255),
+                Forms\Components\Toggle::make('enabled')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('currency_code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('opening_balance')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('bank_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('bank_phone')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('enabled')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAccounts::route('/'),
+            'create' => Pages\CreateAccount::route('/create'),
+            'edit' => Pages\EditAccount::route('/{record}/edit'),
+        ];
+    }
+}
--- database/migrations/2023_12_26_003955_create_accounts_table.php
+++ database/migrations/2023_12_26_003955_create_accounts_table.php
@@ -15,15 +15,12 @@ return new class extends Migration
 
         Schema::create('accounts', function (Blueprint $table) {
             $table->integer('id')->primary();
-            $table->integer('company_id');
-            $table->foreign('company_id')->references('id')->on('companies');
             $table->string('name');
             $table->string('number');
             $table->string('currency_code');
             $table->double('opening_balance');
             $table->string('bank_name')->nullable();
             $table->string('bank_phone')->nullable();
-            $table->text('bank_address')->nullable();
             $table->tinyInteger('enabled');
             $table->timestamp('created_at')->nullable();
             $table->timestamp('updated_at')->nullable();
